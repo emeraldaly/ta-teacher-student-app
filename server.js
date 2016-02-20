862c2e0359173058af8f5789faca755155c2ee64
Alyson <emerald.aly@gmail.com>
2016-02-19 22:44:36 -0500

    added add user code

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var expressHandlebars = require('express-Handlebars');
+var exphbs = require('express-handlebars');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
+var session = require("express-session");
 
 var connection = new Sequelize('class_app_db', 'root', {
 
 });
 
-var PORT = 8080
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
@@ -16,5 +17,81 @@ app.engine('handlebars', expressHandlebars({
 }));
 
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({
+  }));
+}
 
-console.log("Listening on port %s", PORT); 
+//create student in db
+var student = connection.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    len: {
+      args: [1, 15],
+      msg: 'Please enter your first name',
+          }
+    }
+  },
+
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: {
+        args: [1, 20],
+        msg: 'Please enter your last name',
+      }
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpy: true,
+      isEmail: true,
+      msg: 'Please enter your email address',  
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: true,
+        len: {
+          args: [1, 20],
+          msg: 'Please enter your password',
+        }
+      }
+    },
+  }
+
+})
+
+app.use(session({
+  secret: "my super secret",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 *30
+  },
+  saveUninitialized: true,
+  resave: false
+}));
+
+app.get("/", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req,res){
+  res.render("login");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+//query the db to see if user is student or instructor and render correct page
+app.post("/login", function(req,res){
+  res.render("students");
+});
+app.listen(PORT, function() {
+  console.log("Listening on port %s", PORT); 
+});
